405f42fe9c87552cf329d198f747c569e3ee6f7e
NazareSahil <sahilnazare31@gmail.com>
2022-04-14 15:21:34 +0530

    upload0.0

--- code/heart_analysis/core/views.py
+++ code/heart_analysis/core/views.py
@@ -2,6 +2,10 @@ from urllib import response
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from keras.models import load_model
+import librosa
+import numpy as np
+
 # Create your views here.
 
 
@@ -10,7 +14,26 @@ def homepage(req):
 
 
 def temp(req):
-    button = req.POST["textArea"]
-    inputFile = req.POST["inputFile"]
+    # button = req.POST["textArea"]
+    # inputFile = req.POST["inputFile"]
 
-    return render(req,"temporary.html",context = {"variable": 25})
+    # print(req.FILES)
+    file = req.FILES["inputFile"]
+    y, sr = librosa.load(file, sr=16000)
+    print(y.shape)
+    mfcc = np.mean(librosa.feature.mfcc(y, sr, n_mfcc=40).T, axis=0)
+    print(mfcc.shape)
+    res = LSTM(mfcc)
+    print(res)
+    context = {
+        "res" : res
+    }
+    return render(req, "temporary.html",context=context)
+
+    # return render(req,"temporary.html",context = {"variable": 25})
+
+def LSTM(mfcc):
+    path = 'D:\Python Projects\HeartBeatAnalysis\LSTM\model_checkpoints.hdf5'
+    model = load_model(path)
+    predictions = model.predict(mfcc)
+    return predictions
