d2c6409a76cfdc3256024acce154adba88d32375
NazareSahil <sahilnazare31@gmail.com>
2022-04-14 22:31:00 +0530

    val_idx

--- code/heart_analysis/core/views.py
+++ code/heart_analysis/core/views.py
@@ -20,19 +20,32 @@ def temp(req):
     # print(req.FILES)
     file = req.FILES["inputFile"]
     y, sr = librosa.load(file, sr=16000)
-    print(y.shape)
+    # print(y.shape)
     mfccs = np.mean(librosa.feature.mfcc(y,sr,n_mfcc=40).T,axis=0)
-    print(mfccs.shape)
+    # print(mfccs.shape)
     mfccs.reshape((-1,1))
-    print(mfccs.shape)
+    # print(mfccs.shape)
     mfccs = np.reshape(mfccs, (1,40,1))
-    result = LSTM(mfccs)
-    return render(req, "temporary.html",context={"shape":mfccs.shape,"result":result,"y":y})
+    val, idx = LSTM(mfccs)
+    # print(type(result))
+    return render(req, "temporary.html",context={"category": idx, "value": val})
 
 
 
 def LSTM(mfcc):
-    model = load_model(r"C:\Users\adnan\OneDrive\Desktop\HeartAnalysis\Data\model_checkpoints.hdf5")
+    # model = load_model(r"C:\Users\adnan\OneDrive\Desktop\HeartAnalysis\Data\model_checkpoints.hdf5")
+    model = load_model(r"D:\Python Projects\HeartBeatAnalysis\LSTM\model_checkpoints.hdf5")
     predictions = model.predict(mfcc)
-    print(mfcc.shape)
-    return predictions
+    # print(mfcc.shape)
+    category = {
+        0: "EXTRASTOLE/EXTRAHLS",
+        1: "MURMUR",
+        2: "NORMAL"
+    }
+    
+    idx, val = -1, 0
+    for pred in range(len(predictions[0])):
+        if predictions[0][pred]*100 > val*100:
+            val, idx = predictions[0][pred], category[pred]
+
+    return val*100, idx
