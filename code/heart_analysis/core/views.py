55de22fa874a64fdc29493f220821b68a3a22b48
Adnan Shaikh <aa2232783@gmail.com>
2022-04-16 22:59:24 +0530

    white space removed

--- code/heart_analysis/core/views.py
+++ code/heart_analysis/core/views.py
@@ -1,11 +1,11 @@
-from urllib import response
-from django.http import HttpResponse
-from django.shortcuts import render
 
+from django.shortcuts import render
+import io
 from keras.models import load_model
 import librosa
 import numpy as np
-
+import os
+from heart_analysis.settings import BASE_DIR
 # Create your views here.
 
 
@@ -18,12 +18,11 @@ def temp(req):
     # inputFile = req.POST["inputFile"]
 
     # print(req.FILES)
-    file = req.FILES["inputFile"]
+    file = io.BytesIO(req.FILES["inputFile"].read())
+    print(file)
     y, sr = librosa.load(file, sr=16000)
-    # print(y.shape)
-    mfccs = np.mean(librosa.feature.mfcc(y,sr,n_mfcc=40).T,axis=0)
+    mfccs = np.mean(librosa.feature.mfcc(y,sr=sr,n_mfcc=40).T,axis=0)
     # print(mfccs.shape)
-    mfccs.reshape((-1,1))
     # print(mfccs.shape)
     mfccs = np.reshape(mfccs, (1,40,1))
     val, idx = LSTM(mfccs)
@@ -33,16 +32,15 @@ def temp(req):
 
 
 def LSTM(mfcc):
-    # model = load_model(r"C:\Users\adnan\OneDrive\Desktop\HeartAnalysis\Data\model_checkpoints.hdf5")
-    model = load_model(r"D:\Python Projects\HeartBeatAnalysis\LSTM\model_checkpoints.hdf5")
+    model = load_model(os.path.join(BASE_DIR,"model_checkpoints.hdf5"))
+    #model = load_model(r"D:\Python Projects\HeartBeatAnalysis\LSTM\model_checkpoints.hdf5")
     predictions = model.predict(mfcc)
-    # print(mfcc.shape)
     category = {
         0: "EXTRASTOLE/EXTRAHLS",
         1: "MURMUR",
         2: "NORMAL"
     }
-    
+    print(predictions)
     idx, val = -1, 0
     for pred in range(len(predictions[0])):
         if predictions[0][pred]*100 > val*100:
